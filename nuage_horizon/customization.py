1a3d71d9f94bb184743385f75b98827c651aa232
Wim De Clercq <Wim.De_Clercq@alcatel-lucent.com>
2015-05-26 14:45:55 +0200

    Improved the way Nuage code hooks static content (css/js/images) into horizon.
    First attempt at branding.

--- nuage_horizon/customization.py
+++ nuage_horizon/customization.py
@@ -1,20 +1,25 @@
-from dashboards.project import routers
-from dashboards.project import networks
+from dashboards.admin.networks import subnets # noqa
+from dashboards.project import applications # noqa
+from dashboards.project import application_domains # noqa
+from dashboards.project import application_services # noqa
+from dashboards.project import dashboard # noqa
 from dashboards.project import gateways # noqa
 from dashboards.project import gateway_vlans # noqa
-from dashboards.project.networks import subnets
-from dashboards.admin.networks import subnets
-from dashboards.project import applications # noqa
-from dashboards.project import application_services # noqa
-from dashboards.project import application_domains # noqa
-from dashboards.project import dashboard # noqa
 from dashboards.project import instances # noqa
+from dashboards.project import networks # noqa
+from dashboards.project.networks import subnets # noqa
+from dashboards.project import routers # noqa
 
 import os
 
-from horizon import loaders
+from django.conf import settings
 
-root_dir = os.path.dirname(__file__)
-static_dir = os.path.join(root_dir, "static")
-loaders.panel_template_dirs['nuage/js'] = static_dir
-loaders.panel_template_dirs['nuage/css'] = static_dir
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+# Add nuage_horizon/templates to the Django template_dirs.
+# Add it first in the list so nuage templates take priority over the others.
+settings.TEMPLATE_DIRS = ((os.path.join(ROOT_PATH, 'templates'),)
+                          + settings.TEMPLATE_DIRS)
+# Add nuage_horizon/static to the Django static_dirs.
+# Usage like <script src='{{ STATIC_URL }}nuage/js/NuageLinkedSelect.js' ...
+settings.STATICFILES_DIRS.append(('nuage', os.path.join(ROOT_PATH, 'static'),))
+settings.SITE_BRANDING = 'Nuage OpenStack'
