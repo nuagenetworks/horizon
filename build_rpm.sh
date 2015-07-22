49786d1b6550b378dddd08c2a3b9d017812feb3f
Nicolas Ochem <Nicolas.Ochem@alcatel-lucent.com>
2015-07-22 14:26:20 -0700

    remove temporary hack to build rpm
    Change-Id: I39d7c82abad48c4fe1a75cb223ba850d10d8e0f8

--- build_rpm.sh
+++ build_rpm.sh
@@ -15,11 +15,6 @@ set -e
 # we handle nightly builds only for now
 OVS_BUILD_NUMBER=$(echo $BUILD_NAME | sed "s/^.*-\(.*\)$/\1/")
 
-# HACK HACK HACK
-# We do not want to depend on the dashboard do build rpm so I remove the dependency in setup.cfg
-sed -i '/\[global\]/d' setup.cfg
-sed -i '/.*setup.*hook.*/d' setup.cfg
-
 ################## Generate the python rpm ####################################
 PBR_VERSION=${OVS_BUILD_NUMBER} python setup.py bdist_rpm --release="nuage_kilo"
 
