8088582f6d2281a9854882672790e5e2f0806c48
Wim De Clercq <Wim.De_Clercq@alcatel-lucent.com>
2017-05-18 14:02:21 +0200

    The network detail view became a view with tabs.
    Used to be a view with multiple tables (subnets, ports) displayed below
    each other. This is now split up in tabs.
    
    Change-Id: I977a5d0c176bbbad90e22dcd56968c77be30a952
    Closes-Bug: OPENSTACK-1779

--- nuage_horizon/dashboards/project/networks/subnets/tabs.py
+++ nuage_horizon/dashboards/project/networks/subnets/tabs.py
@@ -1,5 +1,27 @@
+from horizon import exceptions
+
+from openstack_dashboard import api
 from openstack_dashboard.dashboards.project.networks.subnets \
     import tabs as def_tabs
+from nuage_horizon.dashboards.project.networks.subnets \
+    import tables as nuage_sub_tables
+
+
+class SubnetsTab(def_tabs.SubnetsTab):
+    table_classes = (nuage_sub_tables.NuageSubnetsTable,)
+
+    def get_subnets_data(self):
+        try:
+            network_id = self.tab_group.kwargs['network_id']
+            fields = ['name', 'id', 'cidr', 'ip_version', 'gateway_ip',
+                      'vsd_managed']
+            subnets = api.neutron.subnet_list(
+                self.request, network_id=network_id, fields=fields)
+        except Exception:
+            subnets = []
+            msg = _('Subnet list can not be retrieved.')
+            exceptions.handle(self.request, msg)
+        return subnets
 
 
 class OverviewTab(def_tabs.OverviewTab):
