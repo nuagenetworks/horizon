f65dea1589177aefdaa73c3db878a63ef5df932e
Tom Stappaerts <tom.stappaerts@nokia.com>
2019-09-11 13:27:26 +0200

    Change http response type to json
    Change http response type to application/json for all
    json responses instead of text/html.
    
    Change-Id: I6cd6922a24426a4b2119edbcd18f81c7ee047af1
    Closes-Bug: OPENSTACK-2697
    (cherry picked from commit 911795763e24d355cb196601cd5e8e6252f8c80d)

--- nuage_horizon/dashboards/project/networks/views.py
+++ nuage_horizon/dashboards/project/networks/views.py
@@ -80,8 +80,7 @@ class NuageCreateView(original.CreateView):
                                                            validate_step_end)
             data['hidden_fields'] = self.add_hidden_fields(workflow,
                                                            validate_step_end)
-            return http.HttpResponse(json.dumps(data),
-                                     content_type="application/json")
+            return http.JsonResponse(data)
         if not workflow.is_valid():
             return self.render_to_response(context)
         try:
@@ -101,7 +100,7 @@ class NuageCreateView(original.CreateView):
             field_id = self.request.META["HTTP_X_HORIZON_ADD_TO_FIELD"]
             data = [self.get_object_id(workflow.object),
                     self.get_object_display(workflow.object)]
-            response = http.HttpResponse(json.dumps(data))
+            response = http.JsonResponse(data, safe=False)
             response["X-Horizon-Add-To-Field"] = field_id
             return response
         next_url = self.request.POST.get(workflow.redirect_param_name, None)
@@ -111,7 +110,7 @@ class NuageCreateView(original.CreateView):
 def organization_data(request):
     org_list = neutron.vsd_organisation_list(request)
     org_list = [org.to_dict() for org in org_list]
-    response = http.HttpResponse(json.dumps(org_list, ensure_ascii=False))
+    response = http.JsonResponse(org_list, safe=False)
     return response
 
 
@@ -119,7 +118,7 @@ def domain_data(request):
     org_id = request.GET.get('org_id', None)
     dom_list = neutron.vsd_domain_list(request, vsd_organisation_id=org_id)
     dom_list = [org.to_dict() for org in dom_list]
-    response = http.HttpResponse(json.dumps(dom_list, ensure_ascii=False))
+    response = http.JsonResponse(dom_list, safe=False)
     return response
 
 
@@ -127,7 +126,7 @@ def zone_data(request):
     dom_id = request.GET.get('dom_id', None)
     zone_list = neutron.vsd_zone_list(request, vsd_domain_id=dom_id)
     zone_list = [zone.to_dict() for zone in zone_list]
-    response = http.HttpResponse(json.dumps(zone_list, ensure_ascii=False))
+    response = http.JsonResponse(zone_list, safe=False)
     return response
 
 
@@ -135,7 +134,7 @@ def subnet_data(request):
     zone_id = request.GET.get('zone_id', None)
     subnet_list = neutron.vsd_subnet_list(request, vsd_zone_id=zone_id)
     subnet_list = [subnet.to_dict() for subnet in subnet_list]
-    response = http.HttpResponse(json.dumps(subnet_list, ensure_ascii=False))
+    response = http.JsonResponse(subnet_list, safe=False)
     return response
 
 
