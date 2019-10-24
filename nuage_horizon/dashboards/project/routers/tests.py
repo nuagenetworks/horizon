85d1c6c6019d7a4f54b526f0040ae01bc9145910
Tom Stappaerts <tom.stappaerts@nokia.com>
2019-10-24 18:17:45 +0200

    Make Horizon python3 compatible
    Change-Id: I54440191b7ca54670553868d55beb6d273d21f99
    Closes-Bug: OPENSTACK-2698
    (cherry picked from commit 6b4b81c7a5ca948fbdcb1fdd04bf8021290572df)
    (cherry picked from commit 329f275db78a8a2b94ac2262ede0cbc55bfd4628)

--- nuage_horizon/dashboards/project/routers/tests.py
+++ nuage_horizon/dashboards/project/routers/tests.py
@@ -11,7 +11,7 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django import http
 
 from mox import IsA  # noqa
